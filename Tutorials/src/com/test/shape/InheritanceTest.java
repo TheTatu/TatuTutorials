c6eed590502d31cab071ac63d96a59289226f990
Yogi <syog@live.com>
2021-03-21 22:19:53 +0530

    minor changes

--- Tutorials/src/com/test/shape/InheritanceTest.java
+++ Tutorials/src/com/test/shape/InheritanceTest.java
@@ -3,7 +3,7 @@ package com.test.shape;
 import com.test.inheritence.Circle;
 import com.test.inheritence.Triangle;
 
-public class InheritenceTest {
+public class InheritanceTest {
 	public static void main(String[] args) {
 		Triangle t = new Triangle("T1", 20, 10);
 		System.out.println(t.area());
